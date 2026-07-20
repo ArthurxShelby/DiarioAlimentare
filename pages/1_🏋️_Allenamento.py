74e0e407866413be5f18ffa32617144afcb1f9a1
ArthurxShelby <138934594+ArthurxShelby@users.noreply.github.com>
2026-07-20 12:00:24 +0200

    Create 1_🏋️_Allenamento.py

---
+++ pages/1_🏋️_Allenamento.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Allenamento", page_icon="🏋️")
+st.title("🏋️ Registro Allenamenti")
+st.write("Benvenuto nella sezione allenamento! Qui inseriremo la logica per i tuoi workout.")
